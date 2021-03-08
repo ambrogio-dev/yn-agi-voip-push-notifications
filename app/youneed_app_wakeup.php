7a02e45d419a7eed41a24004090bb39c1d7a839b
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-03-08 15:07:38 +0100

    added support for HTTP Basic Auth

--- app/youneed_app_wakeup.php
+++ app/youneed_app_wakeup.php
@@ -8,6 +8,7 @@ define("AGIBIN_DIR", "/var/lib/asterisk/agi-bin");
 include(AGIBIN_DIR."/phpagi.php");
 
 $endpoint = 'https://beta.youneed.it/phonenotifications/incoming_call_notification';
+$configuration_path = '/etc/asterisk/nethcti_push_configuration.json';
 
 $agi = new AGI();
 $call_id = $agi->request['agi_uniqueid'];
@@ -38,7 +39,7 @@ foreach ($extensions as $index => $extension) {
 }
 
 try {
-   $serverCredentials = json_decode(file_get_contents('/etc/asterisk/nethcti_push_configuration.json'),TRUE);
+   $serverCredentials = json_decode(file_get_contents($configuration_path ),TRUE);
    if (is_null($serverCredentials)) {
       syslog(LOG_ERR, "Error reading public hostname.");
       Throw new Exception('Error reading public hostname.');
@@ -48,7 +49,19 @@ try {
    exit(1);
 }
 
-$public_hostname = $serverCredentials['Host']; // TODO: validate this mandatory field
+$public_hostname = $serverCredentials['Host'];
+$server_user = $serverCredentials['SystemId'];
+$server_secret = $serverCredentials['Secret'];
+
+if (is_null($public_hostname) || empty($public_hostname)) {
+   syslog(LOG_ERR, "Undefined hostname in $configuration_path");
+   exit(1);
+}
+
+if (is_null($server_user) || is_null($server_secret) || empty($server_user) || empty($server_secret)) {
+   syslog(LOG_ERR, "Undefined server credentials in $configuration_path");
+   exit(1);
+}
 
 // Usually "$extensions" contains a single element, so sending a HTTP request for every extension should be fine.
 foreach ($extensions as $extension) {
@@ -63,6 +76,7 @@ foreach ($extensions as $extension) {
    curl_setopt($ch, CURLOPT_URL, $url);
    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+   curl_setopt($ch, CURLOPT_USERPWD, $server_user . ':' . $server_secret);
    $response = curl_exec($ch);
    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
    curl_close($ch);
